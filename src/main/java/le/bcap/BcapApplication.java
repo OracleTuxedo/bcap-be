c40ed28ddade0afd11cdcba6065515c8a7e3eb0d
DRucci-88 <d.rucci.2001@gmail.com>
2024-09-18 10:04:28 +0700

    Initial Commit

---
+++ src/main/java/le/bcap/BcapApplication.java
@@ -0,0 +1,13 @@
+package le.bcap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/le/bcap/BcapApplicationTests.java
@@ -0,0 +1,13 @@
+package le.bcap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
