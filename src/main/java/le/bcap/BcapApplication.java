06e41eb4cdf69ec4dad6c494d6c5ec432c7e83f5
DRucci-88 <d.rucci.2001@gmail.com>
2024-09-19 11:44:01 +0700

    BcapApplication

--- src/main/java/le/bcap/BcapApplication.java
+++ src/main/java/le/bcap/BcapApplication.java
@@ -1,13 +1,30 @@
 package le.bcap;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mti.com.telegram.util.InterfaceTelegram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BcapApplication {
 
+	private static final Logger logger = LogManager.getLogger(BcapApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BcapApplication.class, args);
+		try {
+			logger.atLevel(Level.ALL);
+			logger.info("Le Rucco Start Application");
+			System.out.println("HAHAHAHAHAH");
+			InterfaceTelegram.interfaceTuxedo();
+		} catch (Exception e) {
+			logger.info("Le Rucco Failed MAYDAY MAYDAY MAYDAY");
+			throw new RuntimeException(e);
+		}
+
 	}
 
 }
