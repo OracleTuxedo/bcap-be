0460790ba79ecf268fb7ae8a4b6749bc71e1e974
DRucci-88 <d.rucci.2001@gmail.com>
2024-09-20 13:27:51 +0700

    Success parse Unit Req and Res (SED03F075R)

---
+++ src/main/java/mti/ed/ed03/vo/SED03F075ROutSub1Vo.java
@@ -0,0 +1,32 @@
+package mti.ed.ed03.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mti.com.telegram.model.FieldType;
+import mti.com.telegram.model.Kind;
+import mti.com.telegram.model.TrimType;
+import mti.com.telegram.model.annotation.FIELD;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SED03F075ROutSub1Vo {
+
+    @FIELD(kind = Kind.DATA, length = 5, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String prd_cd;
+
+    @FIELD(kind = Kind.DATA, length = 2, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String mnft_cd;
+
+    @FIELD(kind = Kind.DATA, length = 100, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String prd_nm;
+
+    @FIELD(kind = Kind.DATA, length = 5, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String prd_ctgo_cd;
+
+    @FIELD(kind = Kind.DATA, length = 14, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String data_inp_dttm;
+}
