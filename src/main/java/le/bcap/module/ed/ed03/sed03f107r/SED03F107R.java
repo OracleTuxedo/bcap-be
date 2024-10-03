438d8fa77bac7ebf22cba462711ee72cfb2380ab
DRucci-88 <d.rucci.2001@gmail.com>
2024-10-03 11:40:32 +0700

    BaseModuleInterface

--- src/main/java/le/bcap/module/ed/ed03/sed03f107r/SED03F107R.java
+++ src/main/java/le/bcap/module/ed/ed03/sed03f107r/SED03F107R.java
@@ -2,6 +2,7 @@ package le.bcap.module.ed.ed03.sed03f107r;
 
 import jakarta.servlet.http.HttpServletRequest;
 import le.bcap.az.ServiceSupport;
+import le.bcap.module.BaseModuleInterface;
 import mti.com.telegram.exception.TelegramNestedRuntimeException;
 import mti.com.telegram.vo.TelegramUserDataInput;
 import mti.com.telegram.vo.TelegramUserDataOutput;
@@ -11,14 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SED03F107R {
+public class SED03F107R implements BaseModuleInterface<SED03F107ROutVo, SED03F107RInVo> {
     private static final Logger logger = LogManager.getLogger(SED03F107R.class);
 
     @Autowired
     private ServiceSupport support;
 
+    @Override
     public TelegramUserDataOutput<SED03F107ROutVo> call(HttpServletRequest request, SED03F107RInVo inVo, String screenId){
         TelegramUserDataOutput<SED03F107ROutVo> result = TelegramUserDataOutput.<SED03F107ROutVo>builder().build();
+
         SED03F107ROutVo outVo = SED03F107ROutVo.builder().build();
 
         try {
