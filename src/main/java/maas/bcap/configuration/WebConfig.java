63a4d5827be70a80336d55ac50f83ecc2ac41d14
DRucci-88 <d.rucci.2001@gmail.com>
2024-10-04 13:58:10 +0700

    change group id pom.xml

--- src/main/java/maas/bcap/configuration/WebConfig.java
+++ src/main/java/maas/bcap/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package maas.bcap.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,10 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig {
 
     @Bean
+    @NonNull
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")  // Allow CORS on all endpoints
                         .allowedOrigins("http://localhost:5173", "http://localhost:3000")  // Allow the specific origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific methods
--- src/main/java/mti/com/telegram/util/WtcConnector.java
+++ src/main/java/mti/com/telegram/util/WtcConnector.java
@@ -1,8 +1,11 @@
 package mti.com.telegram.util;
 
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class WtcConnector {
