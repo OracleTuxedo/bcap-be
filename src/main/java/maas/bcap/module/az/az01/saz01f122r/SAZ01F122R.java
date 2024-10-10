6552875df6c41a995b96340688dbc64cc6f4528c
Figha Al Azmi <alfigha26@gmail.com>
2024-10-10 17:58:05 +0700

    saz01f122r Module

---
+++ src/main/java/maas/bcap/module/az/az01/saz01f122r/SAZ01F122R.java
@@ -0,0 +1,41 @@
+package maas.bcap.module.az.az01.saz01f122r;
+
+import jakarta.servlet.http.HttpServletRequest;
+import maas.bcap.az.ServiceSupport;
+import maas.bcap.module.BaseModuleInterface;
+import mti.com.telegram.exception.TelegramNestedRuntimeException;
+import mti.com.telegram.vo.TelegramUserDataInput;
+import mti.com.telegram.vo.TelegramUserDataOutput;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SAZ01F122R implements BaseModuleInterface<SAZ01F122ROutVo, SAZ01F122RInVo> {
+    private static final Logger logger = LogManager.getLogger(SAZ01F122R.class);
+
+    @Autowired
+    private ServiceSupport support;
+
+    @Override
+    public TelegramUserDataOutput<SAZ01F122ROutVo> call(HttpServletRequest request, SAZ01F122RInVo inVo, String screenId) {
+        TelegramUserDataOutput<SAZ01F122ROutVo> result = TelegramUserDataOutput.<SAZ01F122ROutVo>builder().build();
+        SAZ01F122ROutVo outVo = SAZ01F122ROutVo.builder().build();
+
+        try {
+            TelegramUserDataInput userDataInput = support.tuxedoHeader(request, this.getClass().getSimpleName(), screenId);
+            result = support.tuxedoTransaction(userDataInput, inVo, outVo);
+        } catch (TelegramNestedRuntimeException e) {
+            logger.info(e.toString());
+            logger.info(e.getMsg());
+        } catch (Exception e) {
+            logger.info(e.toString());
+            logger.info(e.getClass());
+            logger.info(e.getLocalizedMessage());
+            logger.info(e.getMessage());
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/maas/bcap/module/az/az01/saz01f122r/SAZ01F122RInVo.java
@@ -0,0 +1,35 @@
+package maas.bcap.module.az.az01.saz01f122r;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mti.com.telegram.model.FieldType;
+import mti.com.telegram.model.Kind;
+import mti.com.telegram.model.NumberType;
+import mti.com.telegram.model.TrimType;
+import mti.com.telegram.model.annotation.DATATYPE;
+import mti.com.telegram.model.annotation.FIELD;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SAZ01F122RInVo {
+
+    @FIELD(kind = Kind.DATA, length = 8, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String job_date;
+
+    @FIELD(kind = Kind.DATA, length = 24, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String parallel_id;
+
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String error_check;
+
+    @FIELD(kind = Kind.DATA, length = 6, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.LONG, decimal = 0)
+    public long page_size;
+
+    @FIELD(kind = Kind.DATA, length = 256, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String next_key_val;
+}
