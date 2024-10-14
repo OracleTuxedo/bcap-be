fe7618cdb223497a7b5f4b0919470a2946b18755
Figha Al Azmi <alfigha26@gmail.com>
2024-10-14 09:44:38 +0700

    saz01f123r Module

---
+++ src/main/java/maas/bcap/module/az/az01/saz01f123r/SAZ01F123R.java
@@ -0,0 +1,41 @@
+package maas.bcap.module.az.az01.saz01f123r;
+
+import jakarta.servlet.http.HttpServletRequest;
+import maas.bcap.az.ServiceSupport;
+import maas.bcap.module.BaseModuleInterface;
+import mti.com.telegram.exception.TelegramNestedRuntimeException;
+import mti.com.telegram.vo.TelegramUserDataInput;
+import mti.com.telegram.vo.TelegramUserDataOutput;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SAZ01F123R implements BaseModuleInterface<SAZ01F123ROutVo, SAZ01F123RInVo> {
+    private static final Logger logger = LogManager.getLogger(SAZ01F123R.class);
+
+    @Autowired
+    private ServiceSupport support;
+
+    @Override
+    public TelegramUserDataOutput<SAZ01F123ROutVo> call(HttpServletRequest request, SAZ01F123RInVo inVo, String screenId) {
+        TelegramUserDataOutput<SAZ01F123ROutVo> result = TelegramUserDataOutput.<SAZ01F123ROutVo>builder().build();
+        SAZ01F123ROutVo outVo = SAZ01F123ROutVo.builder().build();
+
+        try {
+            TelegramUserDataInput userDataInput = support.tuxedoHeader(request, this.getClass().getSimpleName(), screenId);
+            result = support.tuxedoTransaction(userDataInput, inVo, outVo);
+        } catch (TelegramNestedRuntimeException e) {
+            logger.info(e.toString());
+            logger.info(e.getMsg());
+        } catch (Exception e) {
+            logger.info(e.toString());
+            logger.info(e.getClass());
+            logger.info(e.getLocalizedMessage());
+            logger.info(e.getMessage());
+        }
+
+        return result;
+    }
+}
