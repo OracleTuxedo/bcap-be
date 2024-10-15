76019c06c95fbc7d2341fb0e6f13229664ecbfb3
Nehemia <nehemia.cecio.3@gmail.com>
2024-10-15 16:55:49 +0700

    SAU01F740R - Request Success, Response Error

---
+++ src/main/java/maas/bcap/module/au/au01/sau01f740r/SAU01F740RInVo.java
@@ -0,0 +1,27 @@
+package maas.bcap.module.au.au01.sau01f740r;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mti.com.telegram.model.FieldType;
+import mti.com.telegram.model.Kind;
+import mti.com.telegram.model.TrimType;
+import mti.com.telegram.model.annotation.FIELD;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SAU01F740RInVo {
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String next_page_exist_yn;
+
+    @FIELD(kind = Kind.DATA, length = 256, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String next_key_val;
+
+    @FIELD(kind = Kind.DATA, length = 3, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String mb_no;
+
+    @FIELD(kind = Kind.DATA, length = 19, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String card_no;
+}
---
+++ src/main/java/maas/bcap/module/au/au01/sau01f740r/SAU01F740ROutSub1Vo.java
@@ -0,0 +1,32 @@
+package maas.bcap.module.au.au01.sau01f740r;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mti.com.telegram.model.FieldType;
+import mti.com.telegram.model.Kind;
+import mti.com.telegram.model.NumberType;
+import mti.com.telegram.model.TrimType;
+import mti.com.telegram.model.annotation.DATATYPE;
+import mti.com.telegram.model.annotation.FIELD;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SAU01F740ROutSub1Vo {
+    @FIELD(kind = Kind.DATA, length = 3, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String mb_no;
+
+    @FIELD(kind = Kind.DATA, length = 19, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String card_no;
+
+    @FIELD(kind = Kind.DATA, length = 14, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String bl_reg_dttm;
+
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String bl_reg_rlse_clcd;
+
+    @FIELD(kind = Kind.DATA, length = 14, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String bl_rlse_dttm;
+}
