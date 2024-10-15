8acd44bd86ef22d7645de21f8e34ac17cb8d3c99
Nehemia <nehemia.cecio.3@gmail.com>
2024-10-15 16:57:13 +0700

    SAU01F750R - Request Success, Response Error

---
+++ src/main/java/maas/bcap/module/au/au01/sau01f750r/SAU01F750ROutSub1Vo.java
@@ -0,0 +1,60 @@
+package maas.bcap.module.au.au01.sau01f750r;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mti.com.telegram.model.FieldType;
+import mti.com.telegram.model.Kind;
+import mti.com.telegram.model.NumberType;
+import mti.com.telegram.model.TrimType;
+import mti.com.telegram.model.annotation.DATATYPE;
+import mti.com.telegram.model.annotation.FIELD;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SAU01F750ROutSub1Vo {
+    @FIELD(kind = Kind.DATA, length = 3, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String mb_no;
+
+    @FIELD(kind = Kind.DATA, length = 19, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String card_no;
+
+    @FIELD(kind = Kind.DATA, length = 14, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String auth_dttm;
+
+    @FIELD(kind = Kind.DATA, length = 2, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trms_stat_cd;
+
+    @FIELD(kind = Kind.DATA, length = 6, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String arcv_strc_no;
+
+    @FIELD(kind = Kind.DATA, length = 6, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String auth_no;
+
+    @FIELD(kind = Kind.DATA, length = 4, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String auth_intn_rspn_cd;
+
+    @FIELD(kind = Kind.DATA, length = 20, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trms_dttm;
+
+    @FIELD(kind = Kind.DATA, length = 5, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trms_cnt;
+
+    @FIELD(kind = Kind.DATA, length = 14, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String mb_auth_dttm;
+
+    @FIELD(kind = Kind.DATA, length = 4, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String issur_rspn_cd;
+
+    @FIELD(kind = Kind.DATA, length = 8, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String auth_seq_no;
+
+    @FIELD(kind = Kind.DATA, length = 2, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String auth_sale_knd_cd;
+
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String auth_trns_tp_cd;
+
+}
