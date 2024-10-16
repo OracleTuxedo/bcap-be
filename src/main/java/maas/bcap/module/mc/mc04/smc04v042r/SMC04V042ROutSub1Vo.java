f7e828d8949eda8fdaffb89ac026a388693ec496
Nehemia <nehemia.cecio.3@gmail.com>
2024-10-16 13:41:51 +0700

    SMC04V042R - SMC04V042ROutSub1Vo edit

---
+++ src/main/java/maas/bcap/module/mc/mc04/smc04v042r/SMC04V042ROutSub1Vo.java
@@ -0,0 +1,115 @@
+package maas.bcap.module.mc.mc04.smc04v042r;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import mti.com.telegram.model.FieldType;
+import mti.com.telegram.model.Kind;
+import mti.com.telegram.model.NumberType;
+import mti.com.telegram.model.TrimType;
+import mti.com.telegram.model.annotation.DATATYPE;
+import mti.com.telegram.model.annotation.FIELD;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SMC04V042ROutSub1Vo {
+    @FIELD(kind = Kind.DATA, length = 4, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.LONG, decimal = 0)
+    public long supics_trns_tp_seq_no;
+
+    @FIELD(kind = Kind.DATA, length = 2, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String supics_trns_tp_cd;
+
+    @FIELD(kind = Kind.DATA, length = 8, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String appl_strt_date;
+
+    @FIELD(kind = Kind.DATA, length = 8, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String appl_end_date;
+
+    @FIELD(kind = Kind.DATA, length = 200, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trns_cond_expl;
+
+    @FIELD(kind = Kind.DATA, length = 7, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.DOUBLE, decimal = 4, sign_length = 1, point_length = 1)
+    public double min_day_avg_sale_icr_rt;
+
+    @FIELD(kind = Kind.DATA, length = 4, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String mcc_cd;
+
+    @FIELD(kind = Kind.DATA, length = 6, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trns_chck_strt_time;
+
+    @FIELD(kind = Kind.DATA, length = 6, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trns_chck_end_time;
+
+    @FIELD(kind = Kind.DATA, length = 7, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.LONG, decimal = 0, sign_length = 1, point_length = 1)
+    public long max_trns_cnt;
+
+    @FIELD(kind = Kind.DATA, length = 7, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.DOUBLE, decimal = 4, sign_length = 1, point_length = 1)
+    public double day_max_trns_keyin_rt;
+
+    @FIELD(kind = Kind.DATA, length = 3, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.LONG, decimal = 0, sign_length = 1, point_length = 1)
+    public long non_act_mcnt;
+
+    @FIELD(kind = Kind.DATA, length = 18, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.DOUBLE, decimal = 2, sign_length = 1, point_length = 1)
+    public double day_max_sale_amt;
+
+    @FIELD(kind = Kind.DATA, length = 7, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.LONG, decimal = 0, sign_length = 1, point_length = 1)
+    public long day_max_trns_cnt;
+
+    @FIELD(kind = Kind.DATA, length = 18, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.DOUBLE, decimal = 2, sign_length = 1, point_length = 1)
+    public double day_max_trns_amt;
+
+    @FIELD(kind = Kind.DATA, length = 18, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trns_limt_card_bin_val;
+
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trns_imgmt_yn;
+
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String trns_refs_yn;
+
+    @FIELD(kind = Kind.DATA, length = 1, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String apvl_cmpl_yn;
+
+    @FIELD(kind = Kind.DATA, length = 8, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String apvl_auth_date;
+
+    @FIELD(kind = Kind.DATA, length = 13, type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String apvl_auth_chg_emp_no;
+
+    @FIELD(kind = Kind.DATA, length = 8 , type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String apvl_reg_date;
+
+    @FIELD(kind = Kind.DATA, length = 1 , type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String cncel_yn;
+
+    @FIELD(kind = Kind.DATA, length = 8 , type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String cncel_date;
+
+    @FIELD(kind = Kind.DATA, length = 15 , type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String chng_usr_id;
+
+    @FIELD(kind = Kind.DATA, length = 15 , type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String inp_usr_id;
+
+    @FIELD(kind = Kind.DATA, length = 100 , type = FieldType.STRING, trim = TrimType.RTRIM)
+    public String apvl_mgr_usr_id;
+
+    @FIELD(kind = Kind.DATA, length = 18, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.DECIMAL, decimal = 2, sign_length = 1, point_length = 1)
+    public double min_sv_amt;
+
+    @FIELD(kind = Kind.DATA, length = 3, type = FieldType.NUMBER, trim = TrimType.LTRIM)
+    @DATATYPE(type = NumberType.LONG, decimal = 0, sign_length = 1, point_length = 1)
+    public long time_frame;
+
+}
